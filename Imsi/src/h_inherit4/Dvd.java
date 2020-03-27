de6e37dc2ccc57e9b0f0cc3053b24aab75763698
Canon <Canon@DESKTOP-PL2F7PK>
2020-03-27 10:27:14 +0900

    0327

--- Imsi/src/h_inherit4/Dvd.java
+++ Imsi/src/h_inherit4/Dvd.java
@@ -1,4 +1,4 @@
-package h_inherit3;
+package h_inherit4;
 
 public class Dvd extends Item{
 	String  actor;
@@ -7,12 +7,12 @@ public class Dvd extends Item{
 	public Dvd(){
 		System.out.println("기본 생성자");
 	}
-	public Dvd(int number,String title,String actor, String director){
+	public Dvd(String number,String title,String actor, String director){
 		super.number=number;
 		super.title=title;
 		this.actor=actor;
 		this.director=director;
-		System.out.println("Book 인자 생성자");
+		System.out.println("Dvd 인자 생성자");
 	}
 	public void output() {
 		System.out.println(number);
